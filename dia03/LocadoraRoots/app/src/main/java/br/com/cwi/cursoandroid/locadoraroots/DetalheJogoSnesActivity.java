933efb3cf48dab971248826f4a29d2f038eddcd9
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2017-05-19 19:21:59 -0300

    feat(Picasso): adicionar carregamento de imagens com PIcasso.

--- dia03/LocadoraRoots/app/src/main/java/br/com/cwi/cursoandroid/locadoraroots/DetalheJogoSnesActivity.java
+++ dia03/LocadoraRoots/app/src/main/java/br/com/cwi/cursoandroid/locadoraroots/DetalheJogoSnesActivity.java
@@ -2,7 +2,10 @@ package br.com.cwi.cursoandroid.locadoraroots;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import br.com.cwi.cursoandroid.locadoraroots.models.JogoSnes;
 import br.com.cwi.cursoandroid.locadoraroots.utils.Constantes;
@@ -11,6 +14,7 @@ public class DetalheJogoSnesActivity extends AppCompatActivity {
 
     private JogoSnes jogo;
     private TextView lblTitulo, lblTipo, lblAnoLancamento;
+    private ImageView imgCapaJogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +30,15 @@ public class DetalheJogoSnesActivity extends AppCompatActivity {
         this.lblTipo.setText(this.jogo.getTipo());
         //this.lblAnoLancamento.setText(String.valueOf(this.jogo.getAnoLancamento()));
         this.lblAnoLancamento.setText(this.jogo.getAnoLancamento().toString());
+        Picasso.with(this)
+                .load(this.jogo.getUrlCapaJogo())
+                .into(this.imgCapaJogo);
     }
 
     private void initComponents() {
         this.lblTitulo = (TextView)findViewById(R.id.lblTitulo);
         this.lblTipo = (TextView)findViewById(R.id.lblTipo);
         this.lblAnoLancamento = (TextView)findViewById(R.id.lblAnoLancamento);
+        this.imgCapaJogo = (ImageView)findViewById(R.id.imgCapaJogo);
     }
 }
